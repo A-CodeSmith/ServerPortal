301a8bd779076eb2981739d24a2ef3c7cef15b59
A-CodeSmith <sylace@gmail.com>
2020-03-26 01:34:44 -0400

    Added basic DB and debugging setup
    Removed  unused route "users" leftover from template. Games route will log a document from a mongodb connection.  Added vscode launch config for basic debugging setup.

--- api/routes/games.js
+++ api/routes/games.js
@@ -1,8 +1,32 @@
 var express = require("express");
 var router = express.Router();
+const {MongoClient} = require('mongodb');
 
-router.get("/", function(req, res, next) {
-    res.send("game 1 info");
+router.get("/:gameId/commands/:commandId", function(req, res, next) {
+    res.send(`requested game ${req.params.gameId} with command ${req.params.commandId}`);
 });
+
+router.get("/", async function(req, res, next) {
+    const uri = "mongodb://localhost:27017/servers";
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+    try {
+        await client.connect();
+        await listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+    res.send("ok");
+});
+
+async function listDatabases(client){
+    const collections = await client.db().collections();
+    const cursor = await collections[0].find({});
+    cursor.each(function(err, doc) {
+        if (err) throw err;
+        console.log(doc);
+    });
+};
 
 module.exports = router;
